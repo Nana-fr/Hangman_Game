55d5646b7b27e63dcf936f0e4c70c8d01589ec4d
Anaïs <c7anais@gmail.com>
2021-09-24 11:59:43 +0200

    add comments and fix toUpperCase()'s bug

--- js/main.js
+++ js/main.js
@@ -7,66 +7,72 @@ let points = 7
 // Functions
 
 function generateWord() {
-    return list[Math.floor(Math.random() * list.length)];
+    return list[Math.floor(Math.random() * list.length)]; // computer chooses randomly a word in the list
   }
 
 function secretWord() {
     for (let i=0; i<wordToGuess.length; i++){
-        hiddenWord[i] = " _ ";
-    } i = 0;
+        hiddenWord[i] = " _ "; // transform the chosen word in underscore
+    }
+    i = 0;
     alert(hiddenWord.join(""));
 }
 
 function askPlayerLetter() {
-    let letter = prompt("Choose a letter:").toUpperCase();
-    if (letter === null){
-        return null;
+    let letter = prompt("Choose a letter:");
+    if (letter === null){ // if player clicked on cancel's button, function stops there, player will be redirect to dashboard 
+        return null; // first step
     }
-    while (letter.length != 1){
-        letter = prompt("Choose a letter:").toUpperCase();
+    while (letter.length != 1){ // check if the player enters one letter only
+        letter = prompt("Choose a letter:");
     } return letter;
 }
 
 function checkAnswer() {
-    for (let i=0; i<wordToGuess.length; i++){
-        if (playerLetter == wordToGuess[i]){
-            hiddenWord[i] = playerLetter
+    for (let i=0; i<wordToGuess.length; i++){ // check if the letter chosen by the player is in the word, 
+        if (playerLetter.toUpperCase() == wordToGuess[i]){ // if yes, all the underscores where the letter occurs are replaced by it
+            hiddenWord[i] = playerLetter.toUpperCase();
         }
     }
-    wordToGuess.includes(playerLetter)? points : points--;
+    wordToGuess.includes(playerLetter.toUpperCase())? points : points--;
     i = 0 ;
-    return alert(hiddenWord.join("") + "\nYou still have " + points + " chance(s).");
+    return alert(`${hiddenWord.join("")}\nYou still have ${points} chance(s).`);
 } 
 
 
 // Game
 
 do {
-    var val = prompt("R for rules, P for play, and Q to quit").toUpperCase();
+    var val = prompt("Welcome to the Hangman game.\nPlease enter 'R' to check the rules, 'S' to start the game, and 'Q' to quit."); // dashboard
 switch (val) {
     case "R":
+    case "r":
         alert("rules");
         break;
-    case "P":
+    case "S": // start the game
+    case "s":
         var wordToGuess = generateWord();
         secretWord();
         while (points != 0 && hiddenWord.includes(" _ ")){
             var playerLetter = askPlayerLetter();
-            if (playerLetter === null){
-                points ===0;
+            if (playerLetter === null){ // if the player has clicked on cancel's button previously it will stop the loop and go back to dashboard
+                points === 0; // second step
                 break;
             }
             checkAnswer();  
         }
-        if (playerLetter !== null){
-            points === 0 ? alert("You lost") : alert("Congratulations you win");
+        if (playerLetter === null){ // if the player has clicked on cancel's button previously, he will go back to dashboard, final step
+            alert("Coward!")
+        } else {
+            points === 0 ? alert("You have been hanged.\nGAME OVER") : alert("Congratulations you have found the password.\nYour neck is safe for now.");
         }
-        hiddenWord = []
+        hiddenWord = [] // reset the variables
         points = 7;
         break;
-    case "Q":
-    case null:
-        alert("Thanks for playing");
+    case "Q": // quit the game
+    case "q":
+    case null: // if the player has clicked on cancel's button, it's equal "to quit the game"
+        alert("Thanks for playing.");
         break;
     }
-} while (val !== "Q");
+} while (val !== "Q" && val !== "q" && val !== null);
