83d3bc3a7eaa9b524b4aad788867b24ac7c77b2b
Anaïs <c7anais@gmail.com>
2021-09-24 10:18:21 +0200

    to go further's version

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 // Variables
 
-let list = ["serendipity", "heathen", "tadpole", "pangram", "abracadabra", "flabbergasted"];
-let wordToGuess = list[Math.floor(Math.random() * list.length)];
+let list = ["SERENDIPITY", "HEATHEN", "TADPOLE", "PANGRAM", "ABRACADABRA", "FLABBERGASTED"];
 let hiddenWord = []
 let points = 7
 
 // Functions
 
+function generateWord() {
+    return list[Math.floor(Math.random() * list.length)];
+  }
+
 function secretWord() {
     for (let i=0; i<wordToGuess.length; i++){
         hiddenWord[i] = " _ ";
-    } alert(hiddenWord.join(""))
+    } i = 0;
+    alert(hiddenWord.join(""));
 }
 
 function askPlayerLetter() {
-    let letter = prompt("Choose a letter:");
+    let letter = prompt("Choose a letter:").toUpperCase();
+    if (letter === null){
+        return null;
+    }
     while (letter.length != 1){
-        letter = prompt("Choose a letter:");
+        letter = prompt("Choose a letter:").toUpperCase();
     } return letter;
 }
 
@@ -31,16 +38,35 @@ function checkAnswer() {
     return alert(hiddenWord.join("") + "\nYou still have " + points + " chance(s).");
 } 
 
+
 // Game
 
-secretWord();
-
-while (points != 0 && hiddenWord.includes(" _ ")){
-    var playerLetter = askPlayerLetter();
-    checkAnswer();
-}
-
-points === 0 ? alert("You lost") : alert("Congratulations you win");
-
-
-
+do {
+    var val = prompt("R for rules, P for play, and Q to quit").toUpperCase();
+switch (val) {
+    case "R":
+        alert("rules");
+        break;
+    case "P":
+        var wordToGuess = generateWord();
+        secretWord();
+        while (points != 0 && hiddenWord.includes(" _ ")){
+            var playerLetter = askPlayerLetter();
+            if (playerLetter === null){
+                points ===0;
+                break;
+            }
+            checkAnswer();  
+        }
+        if (playerLetter !== null){
+            points === 0 ? alert("You lost") : alert("Congratulations you win");
+        }
+        hiddenWord = []
+        points = 7;
+        break;
+    case "Q":
+    case null:
+        alert("Thanks for playing");
+        break;
+    }
+} while (val !== "Q");
